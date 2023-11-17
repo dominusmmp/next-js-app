e678b85036617fde1843654a0110c5f504495979
dominusmmp <dominusmmp@gmail.com>
2023-11-17 04:57:40 -0600

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/_components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,5 +21,46 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, matchUtilities, theme, addBase }) => {
+      addBase({
+        html: {
+          height: "-webkit-fill-available !important",
+        },
+        body: {
+          "min-height": "-webkit-fill-available !important",
+        },
+      });
+
+      addVariant('search-cancel', '&::-webkit-search-cancel-button');
+
+      matchUtilities(
+        {
+          direction: value => ({
+            direction: value,
+          }),
+        },
+        {
+          values: {
+            ltr: 'ltr',
+            rtl: 'rtl',
+            inherit: 'inherit',
+            initial: 'initial',
+            revert: 'revert',
+            'revert-layer': 'revert-layer',
+            unset: 'unset',
+          },
+        }
+      );
+
+      matchUtilities(
+        {
+          'letter-spacing': value => ({
+            letterSpacing: value,
+          }),
+        },
+        { values: theme('letterSpacing') }
+      );
+    }),
+  ],
 }
